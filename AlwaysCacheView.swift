68bb59043a5a53f2ccce7d432fa33d63fcc77b7b
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2018-03-23 17:22:03 -0700

    Adding UIViewDidAppear

---
+++ AlwaysCacheView.swift
@@ -0,0 +1,20 @@
+//
+//  AlwaysCacheView.swift
+//  FunctionalViews
+//
+//  Created by Shaheen Ghiassy on 3/23/18.
+//
+
+import UIKit
+
+public class AlwaysCacheView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ViewCache.swift
@@ -0,0 +1,14 @@
+//
+//  ViewCache.swift
+//  FunctionalViews
+//
+//  Created by Shaheen Ghiassy on 3/23/18.
+//
+
+import UIKit
+
+public class ViewCache: NSObject {
+    public let instance: ViewCache = ViewCache()
+
+    
+}
