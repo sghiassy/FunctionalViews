d4d42022cc4be1967f32f7d0487c7f78bc658b9e
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2018-03-24 21:03:11 -0700

    Draw function called

--- Example/FunctionalViews/ViewController.swift
+++ Example/FunctionalViews/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import FunctionalViews
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CALayerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +25,24 @@ class ViewController: UIViewController {
         let image = UIImage(named: "snowman")
 
         let blueLayer = CALayer()
-        blueLayer.contents = image?.cgImage
-        blueLayer.borderColor = UIColor.red.cgColor
-        blueLayer.borderWidth = 1.0
+//        blueLayer.contents = image?.cgImage
+//        blueLayer.borderColor = UIColor.red.cgColor
+//        blueLayer.borderWidth = 1.0
 
         blueLayer.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        blueLayer.backgroundColor = UIColor.blue.cgColor
+//        set controller as layer delegate
+        blueLayer.delegate = self    //ensure that layer backing image uses correct scale
+        blueLayer.contentsScale = UIScreen.main.scale
+        //add layer to our view
         self.view.layer.addSublayer(blueLayer)
+        blueLayer.setNeedsDisplay()
+    }
+
+    func draw(_ layer: CALayer, in ctx: CGContext) {
+        ctx.setLineWidth(10.0)
+        ctx.setStrokeColor(UIColor.red.cgColor)
+        ctx.strokeEllipse(in: layer.bounds)
     }
 
 }
